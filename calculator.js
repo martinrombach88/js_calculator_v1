c1d9bb297f45a71a80f63d5250572e730c257d28
martinrombach88 <martinrombach88@gmail.com>
2023-09-18 17:58:42 +0900

    calculator with logical error for single numbers

--- calculator.js
+++ calculator.js
@@ -1,42 +1,55 @@
 let calc = prompt("Please enter your calculation");
 
 let b = 0; //base
-let n = 0; //newNum
-let baseChanged = false;
+let n = 0; //newNum (for multi operator operations)
 let o = ""; //operator
-
-for (let current in calc) {
-	let c = calc[current];
-
-	//aim for baseChanged ternary that calls a switch?
-
-	//if !baseChanged
-	//if c == num && b = 0, b = c.
-	if (!isNaN(c) && !baseChanged) {
-		b = c;
-		baseChanged = true;
-		//if c == num && b = 0, invalid
-	} else if (isNaN(c) && !baseChanged) {
-		alert("Invalid calculation. Enter a number first.");
-		break;
-	}
-
-	//if baseChanged
-
-	//if c == string && t != 0, o = c.
-}
-alert(`b: ${b}, baseChanged: ${baseChanged}`);
-
-//each iteration
+let baseChanged = false;
+let operatorChanged = false;
 
 const calculate = (operator, base, newnum) => {
 	switch (operator) {
 		case "+":
-			return base + newnum;
+			return parseFloat(base) + parseFloat(newnum);
+		case "-":
+			return parseFloat(base) - parseFloat(newnum);
+		case "*":
+			return parseFloat(base) * parseFloat(newnum);
+		case "/":
+			return parseFloat(base) / parseFloat(newnum);
 		default:
-			return "something went wrong";
+			return -1;
 	}
 };
-//if isNan, run switch. 2 nums, run method
 
-//check if c is operator
+//you are iterating over the letters. so this only
+//calculates single numbers
+
+//you need to split the calc string into nums and strings
+
+for (let current in calc) {
+	let c = calc[current];
+
+	//if !baseChanged
+	//if c == num && b = 0, b = c.
+	if (!baseChanged) {
+		if (!isNaN(c)) {
+			b = c;
+			baseChanged = true;
+			//if c == num && b = 0, invalid
+		} else {
+			alert("Invalid calculation. Enter a number first.");
+			break;
+		}
+	} else {
+		if (!operatorChanged) {
+			if (isNaN(c)) {
+				o = c;
+				operatorChanged = true;
+			}
+		} else {
+			if (!isNaN(c)) {
+				alert(`${calc} = ${calculate(o, b, c)}`);
+			}
+		}
+	}
+}

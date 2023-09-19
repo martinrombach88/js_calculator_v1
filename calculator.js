aff468a80e7dc2432faf364ae7f9e0b4c5d57b38
martinrombach88 <martinrombach88@gmail.com>
2023-09-19 21:07:07 +0900

    input regex

--- calculator.js
+++ calculator.js
@@ -1,12 +1,11 @@
 let calcInput = prompt("Please enter your calculation");
-let calcNums = calcInput.match(/\d+/g);
-let calcOps = calcInput.match(/[\+\-\*\//]+/);
+let calc = calcInput.match(/\d+|[\+\-\*\//]+/g);
 //Regex breakdown
+//\d+ all occurrences of a number
+//[\+\-\*\//]+ all occurences of the characters within []
+//multiple assertions joined with |
 
-alert(`nums = ${calcNums}`);
-alert(`ops = ${calcOps}`);
-//make a calcOperators, then merge the arrays (at 1, 3, 5 etc)
-//or make a regex that also grabs the operators!
+//any other issues with input?
 
 let b = 0; //base
 let n = 0; //newNum (for multi operator operations)
@@ -29,36 +28,32 @@ const calculate = (operator, base, newnum) => {
 	}
 };
 
-//you are iterating over the letters. so this only
-//calculates single numbers
+//issue with single strings solved by regex collection
 
-//you need to split the calc string into nums and strings.
-//the logic should operate the same for a split array of numbers and arrays
+for (let current in calc) {
+	let c = calc[current];
 
-// for (let current in calc) {
-// 	let c = calc[current];
-
-// 	//if !baseChanged
-// 	//if c == num && b = 0, b = c.
-// 	if (!baseChanged) {
-// 		if (!isNaN(c)) {
-// 			b = c;
-// 			baseChanged = true;
-// 			//if c == num && b = 0, invalid
-// 		} else {
-// 			alert("Invalid calculation. Enter a number first.");
-// 			break;
-// 		}
-// 	} else {
-// 		if (!newOperator ) {
-// 			if (isNaN(c)) {
-// 				o = c;
-// 				newOperator  = true;
-// 			}
-// 		} else {
-// 			if (!isNaN(c)) {
-// 				alert(`${calc} = ${calculate(o, b, c)}`);
-// 			}
-// 		}
-// 	}
-// }
+	//if !baseChanged
+	//if c == num && b = 0, b = c.
+	if (!baseChanged) {
+		if (!isNaN(c)) {
+			b = c;
+			baseChanged = true;
+			//if c == num && b = 0, invalid
+		} else {
+			alert("Invalid calculation. Enter a number first.");
+			break;
+		}
+	} else {
+		if (!newOperator) {
+			if (isNaN(c)) {
+				o = c;
+				newOperator = true;
+			}
+		} else {
+			if (!isNaN(c)) {
+				alert(`${calc.join("")} = ${calculate(o, b, c)}`);
+			}
+		}
+	}
+}
